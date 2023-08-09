44026fcbbef18945e03c6754fd96ba5b56387a07
Blaster4385 <TheTablaster@aospa.co>
2023-08-09 22:07:59 +0530

    xun: Override 'ro.product.first_api_level' on init
    - Setting this to 32 prevents hardware backed attestation for safteynet as it has been enforced for devices >=33
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -22,5 +22,6 @@ void set_variant_props(const variant_info_t variant) {
         property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
         property_override("bluetooth.device.default_name", variant.marketname, true);
         property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
+        property_override("ro.product.first_api_level", "32");
     }
 }
